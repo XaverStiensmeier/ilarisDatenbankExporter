eb06e5e161cab9e706525564a190df76156cedc5
XaverStiensmeier <xaverstiensmeier@gmx.de>
2022-08-01 14:32:02 +0200

    Larger rework to work with dicts instead of lists. Might include errors.

--- datenbank_to_yaml.py
+++ datenbank_to_yaml.py
@@ -3,39 +3,66 @@ import yaml
 import sys
 import xml.etree.ElementTree as ET
 
+XML_NODE_TEXT = 'inhalt'
+ATTR_COMMENT = '' #'# Attribute'
+
 yaml_dict = {}
 
 if len(sys.argv) != 2:
     sys.stderr.write("Usage: {0} <file>.xml".format(sys.argv[0]))
 
-XML_NODE_CONTENT = 'inhalt'
-ATTR_COMMENT = '' #'# Attribute'
-def yamlout(node, parent):
+def count_all_types(yaml_dict):
+    for key in yaml_dict.keys():
+        print(f"{key}:{len(yaml_dict[key])}")
+    all_keys = []
+    for elem in yaml_dict[key]:
+        all_keys = set(list(all_keys) + list(elem.keys()))
+    print(" "*5 + f"{list(all_keys)}")
+
+def add_type(yaml_dict):
+    for talent in yaml_dict["Talent"]:
+        any_fertigkeit_of_talent = talent["fertigkeiten"].split(",")[0]
+    if talent["fertigkeiten"]:
+        for fertigkeit in yaml_dict["Übernatürliche-Fertigkeit"]:
+            if fertigkeit["name"] == any_fertigkeit_of_talent:
+                for einstellung in yaml_dict["Einstellung"]:
+                    if einstellung["name"] == "Fertigkeiten: Typen übernatürlich":
+                        talent["typ"] = einstellung["inhalt"].split(",")[int(fertigkeit["printclass"])]
+                        break
+                break
+    #else:
+    #    talent["Typ"] = yaml_dict["Einstellung"]["Fertigkeiten: Typen profan"][int(talent["fertigkeiten"].split(",")[0]["printclass"])]
+
+def parse_entry(node):
+    #if node.tag == "talent":
+    #    pass
+    #else:
     nodeattrs = node.attrib
-    children = list(node)
     content = node.text.strip() if node.text else ''
-    if content:
-        if not (nodeattrs or children):
-            # Write as just a name value, nothing else nested
-            parent[node.tag] = content or ''
-            return
-        else:
-            nodeattrs[XML_NODE_CONTENT] = node.text
-    if parent.get(node.tag):
-        parent[node.tag].append({})
-    else: 
-        parent[node.tag] = [{}]
-    parent = parent[node.tag][-1]
+    return nodeattrs, content
+
+def get_child_dict(child_node):
+    nodeattrs = child_node.attrib
+    child_dict = {}
+    child_dict[XML_NODE_TEXT] = child_node.text
     for n,v in nodeattrs.items():
-        parent[n] = v or ''
-    # Write nested nodes
-    for child in children:
-        yamlout(child, parent)
+        child_dict[n] = v or ''
+    return child_dict
+
+def yaml_out(node):
+    yaml_dict = {}
+    children = list(node)
+    
+    for child_node in children:
+        if not yaml_dict.get(child_node.tag):
+            yaml_dict[child_node.tag] = {}
+        yaml_dict[child_node.tag].update({child_node.attrib.get("name") or child_node.text:get_child_dict(child_node)})
+    return yaml_dict
 
 with open(sys.argv[1]) as xmlf:
     tree = ET.parse(xmlf)
-    yamlout(tree.getroot(), yaml_dict)
-    yaml_dict = yaml_dict["Datenbank"][0]
-    print(yaml_dict)
-    with open(sys.argv[1]+".yml", 'w+',encoding="utf-8") as file:
-        documents = yaml.safe_dump(yaml_dict, file,allow_unicode=True)
+
+yaml_dict = yaml_out(tree.getroot())
+
+with open(sys.argv[1]+".yml", 'w+',encoding="utf-8") as file:
+    documents = yaml.safe_dump(yaml_dict, file,allow_unicode=True)
