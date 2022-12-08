9b9e1273e90dc2c5f48162b82cb2584b5169aacc
XaverStiensmeier <xaverstiensmeier@gmx.de>
2022-12-08 10:14:28 +0100

    More Ilaris Specific Code specially for flattening

--- datenbanktoyaml.py
+++ datenbanktoyaml.py
@@ -34,6 +34,47 @@ def add_type(yaml_dict):
     #else:
     #    talent["Typ"] = yaml_dict["Einstellung"]["Fertigkeiten: Typen profan"][int(talent["fertigkeiten"].split(",")[0]["printclass"])]
 
+def prepare_for_discord(yaml_dict):
+    sys.argv[1] = "discord"
+    new_yaml_dict = {}
+    yaml_dict.pop("Einstellung")
+    for key, value in yaml_dict.items():
+        new_yaml_dict.update(value)
+    remove_keys = ["referenzbuch", "variable", "kommentar", "verbilligt", "csBeschreibung", "linkElement", "linkKategorie", "printclass","name"]
+    for key, value in new_yaml_dict.items():
+        for remove_key in remove_keys:
+            value.pop(remove_key, None)
+    return new_yaml_dict
+
+def parse_inhalt(yaml_dict):
+    for key, value in yaml_dict.items():
+        for key_child, value_child in value.items():
+            if value_child.get("kosten"):
+                value_child["ep kosten"] = value_child.pop("kosten")
+            if value_child.get("inhalt"):
+                inhalt = []
+                lines = value_child["inhalt"].split("\n")
+                mod = False
+                for line in lines:
+                    unused = True
+                    for elem in ["Mächtige Magie", "Probenschwierigkeit", "Vorbereitungszeit", "Ziel", "Reichweite", "Wirkungsdauer", "Kosten",
+                                "Fertigkeiten", "Erlernen","Mächtige Liturgie", "Mächtige Anrufung", "Anmerkung"]:
+                        if line.startswith(elem + ": "):
+                            value_child[elem.lower()] = line[len(elem + ": "):]
+                            unused = False
+                            break
+                    if line.startswith("Modifikationen: "):
+                        value_child["modifikationen"] = [line[len("Modifikationen: "):]]
+                        mod = True
+                    elif unused and mod:
+                        value_child["modifikationen"].append(line)
+                    elif unused:
+                        inhalt.append(line)
+                value_child["inhalt"] = "\n".join(inhalt)              
+    return yaml_dict
+    
+
+
 def parse_entry(node):
     #if node.tag == "talent":
     #    pass
@@ -66,6 +107,8 @@ with open(sys.argv[1]) as xmlf:
 yaml_dict = yaml_out(tree.getroot())
 
 add_type(yaml_dict)
+yaml_dict = parse_inhalt(yaml_dict)
+yaml_dict = prepare_for_discord(yaml_dict)
 
 with open(sys.argv[1]+".yml", 'w+',encoding="utf-8") as file:
     documents = yaml.safe_dump(yaml_dict, file,allow_unicode=True)
